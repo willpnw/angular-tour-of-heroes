9d63ba5f1a28c7a580ea7103de7243a86bf8eec0
will <wpatterson@tablesafe.com>
2018-04-25 16:20:01 -0700

    Displaying a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,3 +1,4 @@
+import { HEROES } from '../mock-heroes';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
@@ -7,10 +8,11 @@ import { Hero } from '../hero';
     styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    heroes = HEROES;
+    selectedHero: Hero;
+    onSelect(hero:Hero): void {
+        this.selectedHero = hero;
+    }
 
     constructor() { }
 
