b877a38893dc71a7d5f264e10c8a7293ed71128b
will <wpatterson@tablesafe.com>
2018-04-26 12:47:56 -0700

    Http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule }      from '@angular/core';
-import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
-
-import { HeroService }        from './hero.service';
-import { AppComponent }        from './app.component';
-import { HeroesComponent }     from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from './message.service';
-import { AppRoutingModule } from './/app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserModule }                  from '@angular/platform-browser';
+import { NgModule }                       from '@angular/core';
+import { FormsModule }                    from '@angular/forms'; // <-- NgModel lives here
+import { HeroService }                    from './hero.service';
+import { AppComponent }                   from './app.component';
+import { HeroesComponent }                from './heroes/heroes.component';
+import { HeroDetailComponent }            from './hero-detail/hero-detail.component';
+import { MessagesComponent }              from './messages/messages.component';
+import { MessageService }                 from './message.service';
+import { AppRoutingModule }               from './/app-routing.module';
+import { DashboardComponent }             from './dashboard/dashboard.component';
+import { HttpClientModule }               from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }            from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
     declarations: [
@@ -17,12 +20,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
         HeroesComponent,
         HeroDetailComponent,
         MessagesComponent,
-        DashboardComponent
+        DashboardComponent,
+        HeroSearchComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpClientModule,
+        HttpClientInMemoryWebApiModule.forRoot(
+            InMemoryDataService, { dataEncapsulation: false }
+        )
     ],
     providers: [
         HeroService,
