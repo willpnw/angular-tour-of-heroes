2847ea155bff60e4874b94955f9196e2c6c0ab4c
will <wpatterson@tablesafe.com>
2018-04-26 10:58:22 -0700

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,21 @@ import { HeroesComponent }     from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeroesComponent,
         HeroDetailComponent,
-        MessagesComponent
+        MessagesComponent,
+        DashboardComponent
     ],
     imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        AppRoutingModule
     ],
     providers: [
         HeroService,
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,12 +7,9 @@ import { Hero }              from '../hero';
     templateUrl: './heroes.component.html',
     styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
     heroes: Hero[];
-    selectedHero: Hero;
-    onSelect(hero:Hero): void {
-        this.selectedHero = hero;
-    }
 
     constructor(private heroService: HeroService) { }
 
